3327b36fa3aee3aece89a92d36399575aad86f1c
jmolina518 <jmolina518@gmail.com>
2021-03-11 20:58:07 -0600

    create Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Header from './components/Header'
 function App() {
   return (
     <div className='container'>
-      <h1>Hello From React</h1>
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = ({ title }) => {
+  return (
+    <header>
+      <h1>{title}</h1>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+export default Header
