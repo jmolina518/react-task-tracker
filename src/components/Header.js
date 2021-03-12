652cacc6a54261f71ec798438ec4207d06ca0d66
jmolina518 <jmolina518@gmail.com>
2021-03-11 21:32:44 -0600

    add button function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('click')
+  }
+
   return (
-    <header>
+    <header className='header'>
       <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick} />
     </header>
   )
 }
@@ -11,8 +17,9 @@ const Header = ({ title }) => {
 Header.defaultProps = {
   title: 'Task Tracker',
 }
-export default Header
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
 }
+
+export default Header
