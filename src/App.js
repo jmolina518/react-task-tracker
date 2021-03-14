2be0ab01bc0b06bbe4b0bb40e8ac914bbb4acd18
jmolina518 <jmolina518@gmail.com>
2021-03-13 20:40:59 -0600

    add a random id

--- src/App.js
+++ src/App.js
@@ -24,6 +24,13 @@ function App() {
       reminder: false,
     },
   ])
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -41,7 +48,7 @@ function App() {
   return (
     <div className='container'>
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
