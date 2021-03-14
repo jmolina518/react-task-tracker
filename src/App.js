cd753ef562b213164889497ad2f50e1907c8d044
jmolina518 <jmolina518@gmail.com>
2021-03-13 20:11:00 -0600

    add toggleReminder

--- src/App.js
+++ src/App.js
@@ -28,11 +28,20 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    )
+  }
+
   return (
     <div className='container'>
       <Header />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         'No Tasks To Show'
       )}
