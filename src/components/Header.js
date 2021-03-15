a422fffe1ffa013a5dd9177c239b4c944b3ad5a2
jmolina518 <jmolina518@gmail.com>
2021-03-14 20:15:07 -0500

    add ability to show task creator

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,11 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log('click')
-  }
-
+const Header = ({ title, onAdd }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick} />
+      <Button color='green' text='Add' onClick={onAdd} />
     </header>
   )
 }
